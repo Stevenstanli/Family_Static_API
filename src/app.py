63267b82911826f75e9e4ea8b694587036915980
Steven <stanli-peter19@hotmail.com>
2021-03-25 08:46:33 +0000

    Family_Static_API

--- src/app.py
+++ src/app.py
@@ -25,18 +25,62 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#Metodo GET general para todos los miembros
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
+   
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    
+    return jsonify(members), 200
 
+#Metodo POST
+@app.route('/member', methods=['POST'])
+def addMember():
+    
+    member = request.json
 
-    return jsonify(response_body), 200
+    if not member:
+        return jsonify({"Msj":"Debe enviar un miembro valido!"}), 400
+    
+    jackson_family.add_member(member)
+    
+    return jsonify({"Msj":"Miembro agregado correctamente!"}), 200
+
+#Metodo DELETE
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+
+    member = jackson_family.delete_member(member_id)
+
+    if not member:
+        return jsonify({"Msj":"ID no existe!!"})
+
+    print("Miembro eliminado!!")
+
+    return jsonify(member)
+#Metodo GET por ID
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+
+    member = jackson_family.get_member(member_id)
+
+    if not member:
+        return jsonify({"Msj":" Miembro no existe!!"})
+
+    return jsonify(member)
+
+#Metodo PUT
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+
+    member = request.json
+    
+    if not member:
+        return jsonify({"Msj":"Debe enviar un miembro valido!"}), 400
+    jackson_family.update_member(member_id, member)
+    return jsonify(member)    
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
